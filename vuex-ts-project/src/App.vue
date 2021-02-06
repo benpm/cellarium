94c010466b5480f19425a45527ef03bb2f716f69
Alec Benson <alecbenson21@gmail.com>
2021-02-05 20:20:20 -0700

    Adding typescript vue template

---
+++ vuex-ts-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Automata/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import Automata from './components/Automata.vue';
+
+@Options({
+  components: {
+    Automata,
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vuex-ts-project/src/components/Automata.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="automataWindow">
+      <canvas/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+  },
+})
+export default class HelloWorld extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
